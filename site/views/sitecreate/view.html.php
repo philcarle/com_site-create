aee510f9ede0e5114d81cc177329f9d8b658804a
mhamdi karim <support@@samaweb.fr>
2014-09-22 16:53:40 +0200

    second commit

---
+++ site/views/sitecreate/view.html.php
@@ -0,0 +1,14 @@
+<?php
+
+// No direct access
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.view');
+
+/**
+ * View class for a list of Sitecreate.
+ */
+class SitecreateViewSitecreates extends JView {
+
+
+}
